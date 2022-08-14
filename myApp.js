9fbfa5f08a364685afdaca09b3305a46be7b5734
Realgreat01 <samsonrealgreat@gmail.com>
2022-08-15 00:51:36 +0100

    express,static

--- myApp.js
+++ myApp.js
@@ -2,16 +2,11 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 
-app.get("/", (req, res) => {
+app.get("/public", (req, res) => {
     res.sendFile(__dirname+"/views/index.html")
 })
-
+app.use("/public",express.static(__dirname+"/public"))
 app.listen(5500)
-
-
-
-
-
 
 
 
