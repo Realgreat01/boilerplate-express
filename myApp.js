0c0286427f473df8f76331876aa77380e72bd2f0
Realgreat01 <samsonrealgreat@gmail.com>
2022-08-15 01:02:37 +0100

    response.json method

--- myApp.js
+++ myApp.js
@@ -2,9 +2,21 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 
+app.get("/", ((req, res) =>{ 
+    res.send(`<h1>Hello</h1> <br>
+        <a href="/json"> Json </a> <br>
+        <a href="/public"> public </a>
+        `, 
+    "utf8", "text/html")
+}))
+
+app.get("/json", (req, res)=>{
+    res.json({message: "Hello json"})
+})
 app.get("/public", (req, res) => {
     res.sendFile(__dirname+"/views/index.html")
 })
+
 app.use("/public",express.static(__dirname+"/public"))
 app.listen(5500)
 
